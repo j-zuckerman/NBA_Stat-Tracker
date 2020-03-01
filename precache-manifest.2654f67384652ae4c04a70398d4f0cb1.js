a7a2b8ba2c588f7557d08d8c6e968e1f70d64f62
James Zuckerman <jzuckerman4@gmail.com>
2020-03-01 16:29:18 -0700

    Updates

---
+++ precache-manifest.2654f67384652ae4c04a70398d4f0cb1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "befcdaaea035b5462ff25ec7bd020b63",
+    "url": "/NBA_Scoreboard/index.html"
+  },
+  {
+    "revision": "64df233971514dbdd68e",
+    "url": "/NBA_Scoreboard/static/js/2.e111b062.chunk.js"
+  },
+  {
+    "revision": "13425e2e45fee351b4139959073d7e4f",
+    "url": "/NBA_Scoreboard/static/js/2.e111b062.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9c40c9d2658e1ee51712",
+    "url": "/NBA_Scoreboard/static/js/main.47129e8d.chunk.js"
+  },
+  {
+    "revision": "630e78eb17df1ca72f01",
+    "url": "/NBA_Scoreboard/static/js/runtime-main.f176b8a0.js"
+  }
+]);
